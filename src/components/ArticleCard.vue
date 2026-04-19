c0173b6eabc2c681b8b7be8b5b676f82b689962e
itschool <itschool@tou.edu.kz>
2026-04-19 10:18:32 +0500

    123

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <a href="#" class="card text-decoration-none">
+        <img :src="article.image" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ article.title }}</h5>
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        article: Object
+    }
+}
+</script>
+
+<style scoped>
+    .card img {
+            height: 200px;
+            object-fit: cover;
+        }
+</style>
